900b5512b45a74aa4a1d1711a38fa279d2d64166
Victor Santos <menezes.victorsantos@outlook.com>
2025-09-26 01:13:11 -0300

    Feat: Swagger Ui

--- Program.cs
+++ Program.cs
@@ -1,8 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<UserDb>(opt => opt.UseInMemoryDatabase("AllUsers"));
+builder.Services.AddDbContext<UserDb>(opt => opt.UseInMemoryDatabase("Users"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "Api";
+    config.Title = "FirstApi v0.1";
+    config.Version = "v0.1";
+});
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "Api";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
 
 app.MapGet("/users", async (UserDb db) => await db.Users.ToListAsync());
 
